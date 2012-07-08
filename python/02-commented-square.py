31cf95b0a7c869a4b99d0a58ce6cd0a0a79de8c3
Nathan Dotz <nathan.dotz@gmail.com>
2012-07-08 19:43:54 -0400

    Add comments to square

---
+++ python/02-commented-square.py
@@ -0,0 +1,16 @@
+import turtle
+
+# draw the top of the square
+turtle.forward(100)
+
+# draw the right side of the square
+turtle.right(90)
+turtle.forward(100)
+
+# draw the bottom of the square
+turtle.right(90)
+turtle.forward(100)
+
+# draw the left side
+turtle.right(90)
+turtle.forward(100)
