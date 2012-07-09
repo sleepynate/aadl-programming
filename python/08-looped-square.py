2fbb7c899bbc257e386904fb73e6d7342efacfc8
Nathan Dotz <nathan.dotz@gmail.com>
2012-07-09 15:15:11 -0400

    Add looped versions of earlier shapes

---
+++ python/08-looped-square.py
@@ -0,0 +1,5 @@
+import turtle
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
---
+++ python/10-looped-octagon.py
@@ -0,0 +1,5 @@
+import turtle
+
+for i in range(8):
+    turtle.forward(100)
+    turtle.right(45)
