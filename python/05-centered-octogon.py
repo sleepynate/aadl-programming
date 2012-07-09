623106a1d66110ba27714cb533bce804cb2cc148
Nathan Dotz <nathan.dotz@gmail.com>
2012-07-09 14:02:25 -0400

    Add octogon

---
+++ python/05-centered-octogon.py
@@ -0,0 +1,38 @@
+import turtle
+
+# move the cursor to where the top-right corner should be if we're centered
+turtle.up()
+turtle.goto(-50, 50)
+
+# draw the top of the octogon
+turtle.down()
+turtle.forward(100)
+
+# draw the right upper side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the right side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the right lower side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the bottom of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the left lower side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the left side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
+# draw the left upper side of the octogon
+turtle.right(45)
+turtle.forward(100)
+
