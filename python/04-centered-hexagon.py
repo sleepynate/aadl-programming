a6b95a73e0e144eb60ab11d0761248e7e30ce40b
Nathan Dotz <nathan.dotz@gmail.com>
2012-07-09 13:57:55 -0400

    Add hexagon

---
+++ python/04-centered-hexagon.py
@@ -0,0 +1,30 @@
+import turtle
+
+# move the cursor to where the top-right corner should be if we're centered
+turtle.up()
+turtle.goto(-50, 50)
+
+# draw the top of the hexagon
+turtle.down()
+turtle.forward(100)
+
+# draw the right upper side of the hexagon
+turtle.right(60)
+turtle.forward(100)
+
+# draw the right lower side of the hexagon
+turtle.right(60)
+turtle.forward(100)
+
+# draw the bottom of the hexagon
+turtle.right(60)
+turtle.forward(100)
+
+# draw the right lower side of the hexagon
+turtle.right(60)
+turtle.forward(100)
+
+# draw the right upper side of the hexagon
+turtle.right(60)
+turtle.forward(100)
+
